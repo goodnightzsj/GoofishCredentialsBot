6f611e7aa09bade660c860dd43755e31bd47a2ec
haiyewei <79199415+haiyewei@users.noreply.github.com>
2025-12-26 02:49:03 +0800

    chore: PM2配置明确使用fork模式

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,6 +4,7 @@ module.exports = {
             name: 'goofishcbot',
             script: 'dist/index.js',
             cwd: './',
+            exec_mode: 'fork',
             instances: 1,
             autorestart: true,
             watch: false,
